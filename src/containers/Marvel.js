3fee9824382ef272053e54e9bc4faea221830bf2
Gilberto Marcano <gilberto.marcano.dev@gmail.com>
2017-06-19 17:10:09 -0400

    Basic render api

--- src/containers/Marvel.js
+++ src/containers/Marvel.js
@@ -9,80 +9,66 @@ import Favorite from '../components/Favorite'
 import Footer from '../components/Footer'
 import '../App.css';
 
-const public_key = "30f53129cad6ab1e89e406f839356890";
-const private_key = "b732db679c3e897f041f7f4b82e77cdc6777b58a";
-const ts = new Date().getTime();
-const hash = CryptoJS.MD5(ts + private_key + public_key).toString();
-const url = "http://gateway.marvel.com/v1/public/characters"
-
-let id = [];
-let name = [];
-let img = [];
-
 class Marvel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			heroe: []
+		}
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      Heroe: {
-        id: {},
-        name: {},
-        img: {}
-      }
-    };
-  }
+	componentDidMount() {
+		const public_key = "30f53129cad6ab1e89e406f839356890";
+		const private_key = "b732db679c3e897f041f7f4b82e77cdc6777b58a";
+		const ts = new Date().getTime();
+		const hash = CryptoJS.MD5(ts + private_key + public_key).toString();
+		const url = "http://gateway.marvel.com/v1/public/characters"
+		const heroe = []
 
-  componentDidMount() {
-    axios.get(url+"?apikey="+public_key+"&ts="+ts+"&hash="+hash+"")
-    .then((response) => {
-      let result = response.data.data.results;
-      console.log(this.props);
-      result.map((data) => {
-        id.push(data.id)
-        name.push(data.name)
-        img.push(data.thumbnail.path+"/standard_fantastic."+data.thumbnail.extension)
-        return this.setState(
-          {
-            Heroe:
-            {
-              id: id,
-              name: name,
-              img: img
-            }
-          }
-        )
-      })
-    }).catch((error) => {
-      console.log('err',error);
-    });
-  }
+		axios.get(`${url}?apikey=${public_key}&ts=${ts}&hash=${hash}`).then((response) => {
+			let result = response.data.data.results;
+			result.map((data) => {
+				heroe.push({
+					id: data.id,
+					name: data.name,
+					img: data.thumbnail.path + "/standard_fantastic." + data.thumbnail.extension
+				});
+				return true;
+			})
+			this.setState({heroe})
+		}).catch((error) => {
+			console.log('err', error);
+		});
+	}
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="container-list">
-          <SubHeader />
-          <div>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-          </div>
-        </div>
-        <div className="container-favorite">
-          <Favorite />
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+	renderHeroeList = () => {
+		return this.state.heroe.map((data) => {
+			return (
+				<li>{data.name}</li>
+			)
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<Header/>
+				<div className="container-list">
+					<SubHeader/>
+					<ul>
+						{this.renderHeroeList()}
+					</ul>
+					<div>
+						<Box/>
+					</div>
+				</div>
+				<div className="container-favorite">
+					<Favorite/>
+				</div>
+				<Footer/>
+			</div>
+		);
+	}
 }
 
 export default Marvel;
